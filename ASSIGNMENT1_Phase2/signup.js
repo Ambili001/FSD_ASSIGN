d9800e9a6c2a644f42875b06256c56626578449b
Ambili001 <105069520+Ambili001@users.noreply.github.com>
2022-09-25 23:47:29 +0530

    Add files via upload

---
+++ ASSIGNMENT1_Phase2/signup.js
@@ -0,0 +1,106 @@
+function validate()
+{
+var email=document.getElementById("email");
+var pwd=document.getElementById("pwd");
+var cnfpwd=document.getElementById("cnfpwd");
+var phone=document.getElementById("phone");
+var error1=document.getElementById("error1");
+var error2=document.getElementById("error2");
+var error3=document.getElementById("error3");
+var error4=document.getElementById("error4");
+if(email_validation(email,error1))
+{
+    if (pwd_validation(pwd,error2))
+    {
+        if(cnfpwd_validation(cnfpwd,pwd,error3))
+        {
+            if(phone_validation(phone,error4))
+            {
+                return true;
+            }
+        }
+    }
+}
+return false;
+}
+function email_validation(email,error1)
+{   
+   
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(email.value.match(mailformat))
+    { 
+    error1.innerHTML="";
+    return true;
+    }
+    else
+    {  
+    error1.innerHTML="Invalid Email ID";
+    error1.style.color="red";
+    email.focus();
+    return false;
+    }
+}
+function pwd_validation(pwd,error2)
+{  
+    var error2 = document.getElementById('error2');
+        var strongRegex = new RegExp("^(?=.{14,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
+        var mediumRegex = new RegExp("^(?=.{10,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
+        var enoughRegex = new RegExp("(?=.{8,}).*", "g");
+        var pwd = document.getElementById("pwd");
+        if (false == enoughRegex.test(pwd.value)) {
+            error2.innerHTML = 'Password too short';
+            error2.style.color="red";
+            return false;
+        } else if (strongRegex.test(pwd.value)) {
+            error2.innerHTML = 'Strong';
+            error2.style.color="green" ;
+            return true;
+        } else if (mediumRegex.test(pwd.value)) {
+            error2.innerHTML = 'Medium';
+            error2.style.color="orange";
+            return true;
+        } else {
+            error2.innerHTML = "Weak";
+            error2.style.color="red";
+            return false;
+        }
+        
+    }
+
+
+function cnfpwd_validation(cnfpwd,pwd,error3)
+{   
+    
+    if(cnfpwd.value==pwd.value)
+    {
+        error3.innerHTML="";
+        
+    return true;
+    }
+    else
+    {  
+    error3.innerHTML="Password does not match";
+    error3.style.color="red";
+    cnfpwd.focus();
+    return false;
+    }
+}
+function phone_validation(phone,error4)
+{   
+    var phoneformat = /^\d{3}\d{3}\d{4}|\d{3}-\d{3}-\d{4}|\d{3} \d{3} \d{4}/;
+    if(phone.value.match(phoneformat))
+    {
+        error4.innerHTML="";
+    return true;
+    }
+    else
+    {  
+    error4.innerHTML="Invalid Phone number";
+    error4.style.color="red";
+    phone.focus();
+    return false;
+    }
+}
+
+            
+       
